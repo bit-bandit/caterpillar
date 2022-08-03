af89bca76bd6cf24a37236770344f56ed4890c82
PK <prettykool@protonmail.com>
2022-08-02 20:22:19 -0500

    Fixes on the comment component

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -3,25 +3,43 @@ import { h } from "preact";
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 
+/* API:
+<Comment
+  username={actor.name}
+  avatarURL={actor.icon[0]}
+  date={object.published}
+  commentBody={object.content}
+  likes={likes.length}
+  dislikes={dislikes.length}
+/>
+
+*/
+
 export function Comment(props: any) {
   const d = new Date(props.date);
 
   return (
     <div
-      class={tw`m-4 flex max-w-3xl items-center rounded-3xl py-6 px-5 shadow-lg`}
+      class={tw`m-4 flex max-w-3xl items-center rounded-3xl py-4 px-5 shadow-lg`}
     >
-      <div class={tw`w-24`}>
+      <div class={tw`w-16`}>
         <img class={tw`rounded-full shadow-md`} src={props.avatarURL} />
       </div>
       <div class={tw`flex-none`}>
         <div
-          class={tw`max-w-3xl flex-wrap py-2 px-5 text-2xl font-medium font-light`}
+          class={tw`max-w-4xl flex-wrap py-1 px-5 text-lg`}
           dangerouslySetInnerHTML={{ __html: props.commentBody }}
         />
         <div class={tw`flex px-4`}>
           <div class={tw`px-2 font-bold`}>{props.username}</div>
           <p>-</p>
           <div class={tw`px-2 italic text-slate-500`}>{d.toLocaleString()}</div>
+          <p>-</p>
+          <div class="mx-2 flex">
+            <div class="text-green-700">+{props.likes}</div>
+            <div>/</div>
+            <div class="text-red-700">-{props.dislikes}</div>
+          </div>
         </div>
       </div>
     </div>
