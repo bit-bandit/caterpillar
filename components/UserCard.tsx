5e3437ac6aaf36e10461d9ae202bc302b2bdc75d
PK <prettykool@protonmail.com>
2022-08-08 11:45:49 -0500

    Following page added

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -4,26 +4,38 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 
 export function UserCard(props: any) {
+  let followStr = "Followers"
+  
+  if (props.followers === 1) {
+    followStr = "Follower"
+  }
+  
   let formatedFollowers = Intl.NumberFormat("en-US", {
     notation: "compact",
     maximumFractionDigits: 1,
   }).format(props.followers);
+
+  const u = new URL(props.id);
+  const formattedUsername = `${u.pathname.split('/')[2]}@${u.host}`
+  
   return (
     <div
-      class={tw`p-5 m-4 shadow-md flex items-center max-w-sm rounded-3xl bg-white`}
+      class={tw`p-5 m-4 shadow-md flex items-center max-w-lg rounded-3xl bg-white justify-between m-auto`}
     >
+    <div class={tw`flex`}>
       <div>
         <img class={tw`rounded-full shadow-md w-24`} src={props.icon} />
       </div>
       <div class={tw`m-6 hover:underline`}>
         <a href={props.href}>
-          <h3 class={tw`text-2xl font-bold`}>{}</h3>
-          <p class={tw`text-gray-500`}>r@google.com</p>
+          <h3 class={tw`text-2xl font-bold`}>{props.name}</h3>
+          <p class={tw`text-gray-500`}>{formattedUsername}</p>
         </a>
+      </div>
       </div>
       <div class={tw`text-right`}>
         <h4 class={tw`text-xl font-bold`}>{formatedFollowers}</h4>
-        <p class={tw`text-xs`}>Followers</p>
+        <p class={tw`text-xs`}>{followStr}</p>
       </div>
     </div>
   );
---
+++ routes/u/[id]/following.tsx
@@ -0,0 +1,63 @@
+/** @jsx h */
+import { h } from "preact";
+import { PageProps } from "$fresh/server.ts";
+import { caterpillarSettings } from "../../../settings.ts";
+import { tw } from "@twind";
+import { UserCard } from "../../../components/UserCard.tsx";
+
+export const handler = {
+  async GET(_, ctx) {
+    let res = {};
+    const { id } = ctx.params;
+
+    const userAPI = new URL(`/u/${id}`, caterpillarSettings.apiURL);
+
+    let req = await fetch(userAPI.href);
+
+    res.user = await req.json();
+
+    req = await fetch(res.user.following);
+    req = await req.json();
+
+    for (const url in req.orderedItems) {
+	let f = await fetch(req.orderedItems[url]);
+	f = await f.json();
+
+	let followers = await fetch(f.followers);
+	followers = await followers.json();
+
+	f.followers = followers;
+
+	req.orderedItems[url] = f;
+    }
+
+    res.followers = req.orderedItems
+    return ctx.render(res);
+  },
+};
+
+export default function Following(props: any) {
+  const followers = props.data.followers;
+  console.log(followers)
+  return (
+    <div class={tw`mx-auto max-w-screen-md`}>
+      <div class={tw`text-5xl font-bold leading-tight text-center`}>
+        <h1>Followed by {props.params.id}</h1>
+      </div>
+      <div class={tw`shadow-md p-9 rounded-3xl m-11 max-w-screen-md`}>
+        {followers.map((x) => {
+	  console.log(x.id);
+          return (
+            <UserCard
+	    id={x.id}
+	    followers={x.followers.totalItems}
+	    href={new URL(x.id).pathname}
+	    icon={x.icon[0]}
+	    name={x.name}
+	    />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
