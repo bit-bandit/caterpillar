d6abda3f4223e9d173ca94e50a1a6abcc68c8dc6
PK <prettykool@protonmail.com>
2022-08-21 17:33:15 -0500

    `deno fmt`

--- islands/UserBox.tsx
+++ islands/UserBox.tsx
@@ -5,50 +5,47 @@ import { IS_BROWSER } from "$fresh/runtime.ts";
 import { tw } from "@twind";
 import { SmallSearchBar } from "./SearchBar.tsx";
 import { caterpillarSettings } from "../settings.ts";
-import { SmallUserCard } from '../components/UserCard.tsx';
+import { SmallUserCard } from "../components/UserCard.tsx";
 
 export default function UserBox() {
-
   let [info, setInfo] = useState({ "err": "true", "msg": "No token found" });
 
-  useEffect(async() => {
+  useEffect(async () => {
     let token = "";
-    
+
     const c = await caches.open("parasite");
-    let res = await c.match('/login');
+    let res = await c.match("/login");
 
     if (res !== undefined) {
-      token = await res.text(); 
+      token = await res.text();
     } else {
       return setInfo(info);
     }
-   
-    const checkURL = new URL('/u', caterpillarSettings.apiURL)
-    console.log(`Bearer ${token}`)
+
+    const checkURL = new URL("/u", caterpillarSettings.apiURL);
+    console.log(`Bearer ${token}`);
     res = await fetch(checkURL.href, {
       method: "GET",
       headers: {
         "Authorization": `Bearer ${token}`,
         "Accept": "application/json",
       },
-   });
+    });
 
-   res = await res.json();
+    res = await res.json();
 
-   if (!res.err) {
-     setInfo(res);
-   }
+    if (!res.err) {
+      setInfo(res);
+    }
   }, []);
 
   if (!info.err) {
-     return (
-       <SmallUserCard obj={info} />
-     )
+    return <SmallUserCard obj={info} />;
   } else {
     return (
       <a href="/login">
-      <h3 class={tw`hover:underline`}>Login</h3>
+        <h3 class={tw`hover:underline`}>Login</h3>
       </a>
-    )
+    );
   }
 }
